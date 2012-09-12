c86c0054b35717b412de9c05edb255b298b034ac
Dmitry Stefeev <Stefeev@yandex.ru>
2012-09-12 15:23:52 +0600

    Initial project structure with empty files and 3rdparty libraries

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 therm_controller
 ================
 
-my first microcontroller-based project - simple thermometer (stm32vldiscovery, ds18b20 and winstar 1602 HD44780-compatible LCD)
+1. ABOUT
+therm_controller is my first microcontroller-based project.
+It's application for STM32VLDISCOVERY board measures temperature using DS18b20 sensor and prints results to LCD (HD44780-compatible, Winstar wh1602o is used for testing)
+
+2. COMPILATION
+
+- extract ./3rdparty/libs.tar.bz2 to ./3rdparty (./3rdparty/libs direcorty should appear)
+- install Sourcery CodeBench Lite for ARM EABI
+- edit Makefile:
+	set Sourcery CodeBench tools path to TOOLS_PATH, e.g.:
+	TOOLS_PATH=/home/user/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI
+	
+- type "make" in colsole
+
+3. LOADING TO DEVICE
+TBD
---
+++ libs/ds18b20/inc/ds18b20_lib.h
@@ -0,0 +1,17 @@
+/*************************************************************************************************
+*   This file is part of the simple DS18b20 library. See accompanying file README for details.   *
+*   You can redistribute it and/or modify it under the terms described in LICENSE file.          *
+*   Authors are enumerated in AUTHORS file.                                                      *
+*   Thank you for using this library!                                                            *
+*   P.S. 1 tab == 4 spaces!                                                                      *
+**************************************************************************************************/
+
+#ifndef a1637ba4-d361-4043-8c3c-769783767608
+#define a1637ba4-d361-4043-8c3c-769783767608
+
+/*! \file ds18b20_lib.h
+    \brief A Main and single header for the 18b20 library usage
+*/
+
+
+#endif /* Include guard */
